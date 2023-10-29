db04e3a9688c0f5e41a4e0ef98bbb892e36b60a6
Masato <144527087+3-masato@users.noreply.github.com>
2023-10-29 08:36:01 +0000

    Added basic page for users and books

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,32 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def index
+    @users = User.all
+    @user = current_user
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
   end
 
   private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    user = User.find(params[:id])
+    unless user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
 end
--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,9 @@
 module BooksHelper
+  def empty_index_text(user)
+    if user == current_user
+      "There are no books posted. Share your thoughts on the books you have read!"
+    else
+      "This user has not submitted anything yet."
+    end
+  end
 end
