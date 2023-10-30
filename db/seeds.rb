7c8e99ec8deaa9d43a4897eb7077d240cafc3622
Masato <144527087+3-masato@users.noreply.github.com>
2023-10-30 10:49:09 +0000

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+USER_COUNT = 10
+BOOK_COUNT = 5
+
+
+USER_COUNT.times do |n|
+  user = User.create!(
+    email: "test#{n + 1}@test.com",
+    name: "test user #{n + 1}",
+    password: "testhoge"
+  )
+
+  BOOK_COUNT.times do |n|
+    Book.create!(
+      title: "test title #{n + 1}",
+      body: "test body #{n + 1}",
+      user_id: user.id
+    )
+  end
+end
