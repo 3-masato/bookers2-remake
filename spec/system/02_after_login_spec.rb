1f3c24476a4e6aa0945c3bb9d96ce5f7431d0322
Masato <144527087+3-masato@users.noreply.github.com>
2023-10-30 10:48:29 +0000

    Fixed spec: Double quotation marks allowed

--- spec/system/02_after_login_spec.rb
+++ spec/system/02_after_login_spec.rb
@@ -191,7 +191,7 @@ describe "[STEP2] ユーザログイン後のテスト" do
         open("app/views/layouts/application.html.erb").each do |line|
           strip_line = line.chomp.delete(" ")
           # シングルクォーテーションまたはダブルクォーテーションを許容する正規表現を使用
-          if strip_line.match(/<%=[\s]*javascript_pack_tag[\s]*['"]application['"],[\s]*['"]data-turbolinks-track['"]:[\s]*['"]reload['"]%>/)
+          if strip_line.match?(/<%=[\s]*javascript_pack_tag[\s]*['"]application['"],[\s]*['"]data-turbolinks-track['"]:[\s]*['"]reload['"]%>/)
             is_exist = 1
             break
           end
