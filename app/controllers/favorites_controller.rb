7d86022fdb40ecc9947ad0746c621b8417766249
Masato <144527087+3-masato@users.noreply.github.com>
2023-10-31 12:07:02 +0000

    Fixed feature of favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,18 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_book
 
   def create
-    @book = Book.find(params[:book_id])
     current_user.favorites.create(book_id: @book.id)
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
     fav = current_user.favorites.find_by(book_id: @book.id)
-    fav && fav.destroy # Countermeasures against unauthorized destroy actions
+    fav&.destroy # Countermeasures against unauthorized destroy actions
+  end
+
+  private
+  def set_book
+    @book = Book.find(params[:book_id])
   end
 end
